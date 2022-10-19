5dcc4a0ad0f9175a32fefb99c3a89e394fbba888
David Hodges <dhodges@gmail.com>
2022-10-19 11:57:41 -0400

    initial changes

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -6,16 +6,23 @@ import "./YourToken.sol";
 
 contract Vendor is Ownable {
 
-  //event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+  event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
 
   YourToken public yourToken;
+  
 
   constructor(address tokenAddress) {
     yourToken = YourToken(tokenAddress);
   }
+    uint256 public constant tokensPerEth = 100;
 
   // ToDo: create a payable buyTokens() function:
-
+    function buyTokens() public payable {
+          uint ethSent = msg.value;
+          uint tokensToBuy = ethSent * tokensPerEth;
+          yourToken.transfer(msg.sender, tokensToBuy);
+          emit BuyTokens(msg.sender, msg.value, tokensToBuy);
+    }
   // ToDo: create a withdraw() function that lets the owner withdraw ETH
 
   // ToDo: create a sellTokens(uint256 _amount) function:
--- packages/hardhat/deploy/01_deploy_vendor.js
+++ packages/hardhat/deploy/01_deploy_vendor.js
@@ -10,46 +10,46 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
   // You might need the previously deployed yourToken:
   const yourToken = await ethers.getContract("YourToken", deployer);
 
-  // Todo: deploy the vendor
-  // await deploy("Vendor", {
-  //   from: deployer,
-  //   args: [yourToken.address], // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
-  //   log: true,
-  // });
-  //
-  // const vendor = await ethers.getContract("Vendor", deployer);
+ // Todo: deploy the vendor
+  await deploy("Vendor", {
+    from: deployer,
+    args: [yourToken.address], // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
+    log: true,
+  });
+  
+  const vendor = await ethers.getContract("Vendor", deployer);
 
-  // Todo: transfer the tokens to the vendor
-  // console.log("\n 🏵  Sending all 1000 tokens to the vendor...\n");
-  //
-  // const transferTransaction = await yourToken.transfer(
-  //   vendor.address,
-  //   ethers.utils.parseEther("1000")
-  // );
+  //Todo: transfer the tokens to the vendor
+  console.log("\n 🏵  Sending all 1000 tokens to the vendor...\n");
+  
+  const transferTransaction = await yourToken.transfer(
+    vendor.address,
+    ethers.utils.parseEther("1000")
+  ); 
 
-  //console.log("\n    ✅ confirming...\n");
-  //await sleep(5000); // wait 5 seconds for transaction to propagate
+  console.log("\n    ✅ confirming...\n");
+  await sleep(5000); // wait 5 seconds for transaction to propagate
 
   // ToDo: change address to your frontend address vvvv
-  // console.log("\n 🤹  Sending ownership to frontend address...\n")
-  // const ownershipTransaction = await vendor.transferOwnership("** YOUR FRONTEND ADDRESS **");
-  // console.log("\n    ✅ confirming...\n");
-  // const ownershipResult = await ownershipTransaction.wait();
+ console.log("\n 🤹  Sending ownership to frontend address...\n")
+  const ownershipTransaction = await vendor.transferOwnership(0x9437b1Fd35f280bcFBAd08e7b2b3370C133B5009);
+ console.log("\n    ✅ confirming...\n");
+ const ownershipResult = await ownershipTransaction.wait();
 
-  // ToDo: Verify your contract with Etherscan for public chains
-  // if (chainId !== "31337") {
-  //   try {
-  //     console.log(" 🎫 Verifing Contract on Etherscan... ");
-  //     await sleep(5000); // wait 5 seconds for deployment to propagate
-  //     await run("verify:verify", {
-  //       address: vendor.address,
-  //       contract: "contracts/Vendor.sol:Vendor",
-  //       constructorArguments: [yourToken.address],
-  //     });
-  //   } catch (e) {
-  //     console.log(" ⚠️ Failed to verify contract on Etherscan ");
-  //   }
-  // }
+  //ToDo: Verify your contract with Etherscan for public chains
+  if (chainId !== "31337") {
+    try {
+      console.log(" 🎫 Verifing Contract on Etherscan... ");
+      await sleep(5000); // wait 5 seconds for deployment to propagate
+      await run("verify:verify", {
+        address: vendor.address,
+        contract: "contracts/Vendor.sol:Vendor",
+        constructorArguments: [yourToken.address],
+      });
+    } catch (e) {
+      console.log(" ⚠️ Failed to verify contract on Etherscan ");
+    }
+  }
 };
 
 function sleep(ms) {
