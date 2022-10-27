93038e77a847b5e045782dfb7ae51965d9df3191
David Hodges <dhodges@gmail.com>
2022-10-27 11:40:55 -0400

    Withdraw

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -1,30 +1,46 @@
-pragma solidity 0.8.4;  //Do not change the solidity version as it negativly impacts submission grading
+pragma solidity 0.8.4; //Do not change the solidity version as it negativly impacts submission grading
 // SPDX-License-Identifier: MIT
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./YourToken.sol";
 
 contract Vendor is Ownable {
+    event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+    event Withdraw(address owner, uint256 amountOfETH);
 
-  event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+    YourToken public yourToken;
 
-  YourToken public yourToken;
-  
+    constructor(address tokenAddress) {
+        yourToken = YourToken(tokenAddress);
+    }
 
-  constructor(address tokenAddress) {
-    yourToken = YourToken(tokenAddress);
-  }
     uint256 public constant tokensPerEth = 100;
 
-  // ToDo: create a payable buyTokens() function:
-    function buyTokens() public payable {
-          uint ethSent = msg.value;
-          uint tokensToBuy = ethSent * tokensPerEth;
-          yourToken.transfer(msg.sender, tokensToBuy);
-          emit BuyTokens(msg.sender, msg.value, tokensToBuy);
+    modifier CheckEthInContract(uint256 amt) {
+        require(
+            amt <= address(this).balance,
+            "Not enough eth in contract to withdraw this amount"
+        );
+        _;
     }
-  // ToDo: create a withdraw() function that lets the owner withdraw ETH
 
-  // ToDo: create a sellTokens(uint256 _amount) function:
+    // ToDo: create a payable buyTokens() function:
+    function buyTokens() public payable {
+        uint ethSent = msg.value;
+        uint tokensToBuy = ethSent * tokensPerEth;
+        yourToken.transfer(msg.sender, tokensToBuy);
+        emit BuyTokens(msg.sender, msg.value, tokensToBuy);
+    }
 
+    // ToDo: create a withdraw() function that lets the owner withdraw ETH
+    function withdraw(uint256 ethamt)
+        public
+        onlyOwner
+        CheckEthInContract(ethamt)
+    {
+        address payable to = payable(msg.sender);
+        to.transfer(ethamt);
+        emit Withdraw(msg.sender, ethamt);
+    }
+    // ToDo: create a sellTokens(uint256 _amount) function:
 }
